64593669a46f0a16f1f0edb1c9ab72590792f8ac
laphatradap <laphatradap@disroot.org>
2020-06-09 19:10:46 +0200

    Fix redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,10 @@ import { createStore, applyMiddleware, compose } from "redux";
 import ReduxThunk from "redux-thunk";
 import reducer from "./rootReducer";
 
-const devTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+// const devTools =
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+  const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
 
 const enhancer = compose(applyMiddleware(ReduxThunk), devTools);
 
