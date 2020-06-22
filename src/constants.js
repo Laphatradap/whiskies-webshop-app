bfea923650d9a6ac228fcb25fd87373732a60524
laphatradap <laphatradap@disroot.org>
2020-06-23 00:05:58 +0200

    Add link to Heroku server

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,3 @@
-export const databaseUrl = "http://localhost:4000";
+// export const databaseUrl = "http://localhost:4000";
+
+export const databaseUrl = "https://whiskies-webshop-server.herokuapp.com";
