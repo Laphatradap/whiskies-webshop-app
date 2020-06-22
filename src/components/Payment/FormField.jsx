d2a4c99a24fc8c2f7f3aecfd8712154a08968109
laphatradap <laphatradap@disroot.org>
2020-06-22 23:33:09 +0200

    Integrate Stripe card payment

---
+++ src/components/Payment/FormField.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const FormField = ({ label, type, name, placeholder, required }) => {
+  return (
+    <>
+      <label htmlFor={name}>{label}</label>
+      <input name={name} type={type} placeholder={placeholder} required />
+    </>
+  );
+};
+
+export default FormField;
