36dde421d2bc7eb4c09cf695d5fcdc530768645e
laphatradap <laphatradap@disroot.org>
2020-06-25 16:46:19 +0200

    Add StripeKey in env

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import "./styles/styles.css";
+
 import Homepage from "./components/Homepage";
 import ProductDetail from "./components/ProductList/ProductDetail";
 import PaymentSuccess from "./components/Payment/PaymentSuccess";
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./components/Payment/CheckoutForm";
-import keys from "./config/keys";
-console.log("OUTPUT: keys", keys);
-console.log("env", process.env);
 
-const stripePromise = loadStripe(
-  "pk_test_51Gv0EgEpz8q3eYR4W8D2icEyYlOE4PcgKHm2p3cIufp8O5QWpWo5HLzOBHmebHsf6RLJKpWFuqFIuaio6tGUWGAO00UZgtDM4W"
-);
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
 function App() {
   return (
--- src/config/keys_prod.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-};
